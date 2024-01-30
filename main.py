1c2fc0547cabd2ce924b47da0e85262658c3e898
cehvah <158126098+cehvah@users.noreply.github.com>
2024-01-30 16:32:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 import time
 import pickle
 import os
